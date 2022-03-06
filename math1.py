0d19ed4099930971f83b5814b02e9c06b332616e
Wajid-sb <84681950+Wajid-sb@users.noreply.github.com>
2022-03-07 04:17:58 +0530

    initial commit of math1.py

---
+++ math1.py
@@ -0,0 +1,29 @@
+from decimal import DivisionByZero
+
+
+def square(x,y):
+    return x**y                   # on master branch
+
+# add funtion
+def add(x,y):
+    return x**y
+
+# add funtion
+def add(x,y):
+    return x**y
+
+def divide(x,y):
+    if y==0:
+        return DivisionByZero
+    else:
+        x//y
+
+# added substract method
+def substract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def arrays(a):
+    pass
